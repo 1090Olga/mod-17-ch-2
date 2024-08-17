d9dd938eabc4e6d0acaf96b3b9c2f22f5b9be2f4
1090Olga <168926130+1090Olga@users.noreply.github.com>
2024-08-17 17:19:53 +0300

    Update db.py

--- homework_17_3/app/backend/db.py
+++ homework_17_3/app/backend/db.py
@@ -1,7 +1,4 @@
-# from sqlalchemy import create_engine
-# from sqlalchemy.orm import sessionmaker, DeclarativeBase
 from sqlalchemy import create_engine
-# from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy.orm import sessionmaker
 
@@ -11,20 +8,3 @@ SessionLocal = sessionmaker(bind=engine)
 
 class Base(DeclarativeBase):
     pass
-#
-# from .db import SessionLocal
-#
-# async def get_db():
-#     db = SessionLocal()
-#     try:
-#         yield db
-#     finally:
-#         db.close()
-# Base = declarative_base()
-#
-# class User(Base):
-#     __tablename__ = "user"
-#
-#     id = Column(Integer,primary_key = True)
-#     username = Column(String)
-#     password = Column(String)
